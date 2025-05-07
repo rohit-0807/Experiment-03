034f300e64af23ff4ab00a0847ccd28e0347ee07
rohit-0807 <rohitjeerankalagi07@gmail.com>
2025-05-07 10:52:24 +0530

    save

---
+++ Person.java
@@ -0,0 +1,21 @@
+
+public class Person {
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void show1() {
+        System.out.println("Person Name: " + name + "\nAge: " + age);
+    }
+
+    public static void main(String[] args) {
+        Person p = new Person("Rohit", 21);
+        p.show1();
+        
+    }
+
+}
